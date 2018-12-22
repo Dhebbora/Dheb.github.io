aa9452be4c0c1553555936516d0a00589b708874
Dhebbora <dhebboraleane@gmail.com>
2018-12-22 05:41:31 -0300

    Add style for my webpage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import github from './assets/icons/github.svg';
 import twitter from './assets/icons/twitter.svg';
@@ -6,19 +6,20 @@ import linkedin from './assets/icons/linkedin.svg';
 
  export default app => 
   <div className='container'>
-    <img className='container__img' scr=''></img>
-    <div>
-      <h1 className='content__name'> Nome do Título </h1>
-      <p className='content__description'> Corpo do texto </p>
-      <ul className='content__redessociais'> Redes sociais
-        <li className='content__redessociais__instagram'>
-            <a className='twitter' href='' target='_blanck'><img src={twitter}/></a>         
+    <img className='contener__img'
+    src='https://scontent.frec3-2.fna.fbcdn.net/v/t1.0-9/45655343_1962461960506426_6096299377628807168_n.jpg?_nc_cat=103&_nc_ht=scontent.frec3-2.fna&oh=9488733f9bd11bd49f340d1be14a8288&oe=5C8ED33C'/>
+    <div className='container__content'>
+      <h1 className='content__name'> Dbby </h1>
+      <p className='content__description'> Meu primeiro HTML + CSS </p>
+      <ul className='content__redessociais'>
+        <li className='content__redessociais__item'>
+            <a className='twitter' href='#' target='_blanck'><img src={twitter} alt='Link Twitter'/></a>         
         </li>
-        <li className='content__redessociais__github '>
-            <a className='linkedin' href='' target='_blanck'><img src={linkedin}/></a>
+        <li className='content__redessociais__item'>
+            <a className='linkedin' href='#' target='_blanck'><img src={linkedin}alt='Link linkedin'/></a>
         </li>
-        <li className='content__redessociais__linkedin'>
-            <a className='github' href='' target='_blanck'><img src={github}/></a>
+        <li className='content__redessociais__item'>
+            <a className='github' href='#' target='_blanck'><img src={github}alt='link github'/></a>
         </li>
       </ul>
     </div>
